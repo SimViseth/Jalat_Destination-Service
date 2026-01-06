913d8e608773b65f54d379512d8299b67e04ec8e
viseth <simviseth509@gmail.com>
2026-01-06 23:00:25 +0700

    a

--- src/main/java/com/jalat/destinationservice/feature/province/controller/ProvinceController.java
+++ src/main/java/com/jalat/destinationservice/feature/province/controller/ProvinceController.java
@@ -1,4 +1,15 @@
 package com.jalat.destinationservice.feature.province.controller;
 
+import com.jalat.destinationservice.feature.province.service.ProvinceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/province")
+@RequiredArgsConstructor
 public class ProvinceController {
+
+    private final ProvinceService provinceService;
+
 }
