708f07ae5fb206a45ab78dc0b7f6af4180f2ae75
viseth <simviseth509@gmail.com>
2026-01-24 22:13:50 +0700

    place to eat

--- src/main/java/com/jalat/destinationservice/feature/place_to_eat/controller/PlaceToEatController.java
+++ src/main/java/com/jalat/destinationservice/feature/place_to_eat/controller/PlaceToEatController.java
@@ -1,4 +1,32 @@
 package com.jalat.destinationservice.feature.place_to_eat.controller;
 
+import com.jalat.destinationservice.app.BaseResponse;
+import com.jalat.destinationservice.feature.destination.dto.request.DestinationRequest;
+import com.jalat.destinationservice.feature.destination.dto.response.DestinationResponse;
+import com.jalat.destinationservice.feature.place_to_eat.dto.request.PlaceToEatRequest;
+import com.jalat.destinationservice.feature.place_to_eat.dto.response.PlaceToEatRespoonse;
+import com.jalat.destinationservice.feature.place_to_eat.service.PlaceToEatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/place-to-eat")
+@RequiredArgsConstructor
 public class PlaceToEatController {
+
+    private final PlaceToEatService placeToEatService;
+
+    PostMapping("/create")
+    public BaseResponse<PlaceToEatRespoonse> createPlaceToEat(@RequestBody PlaceToEatRequest placeToEatRequest) {
+        BaseResponse<PlaceToEatRespoonse> placeToEatResponse = placeToEatService.createPlaceToEat(placeToEatRequest);
+        return destinationResponse;
+    }
+
+    @GetMapping("getAll")
+    public BaseResponse<List<DestinationResponse>> getAllDestinations() {
+        BaseResponse<List<DestinationResponse>> destinationResponse = placeToEatService.getAllDestinations();
+        return destinationResponse;
+    }
 }
